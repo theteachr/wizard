7d8955fbe5170a08dc92237faa8396ae242fcb78
theteachr <sop.theteachr@gmail.com>
2024-11-01 03:00:55 +0530

    Burn types

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,18 @@ use std::ops::Not;
 #[derive(Debug)]
 struct SpellError<'a> {
     line: &'a str,
-    err_part: (usize, usize),
-}
-
-#[derive(Debug)]
-struct SpellCheckedLine {
-    line: String,
-    err_parts: Vec<(usize, usize)>,
+    error: &'a str,
 }
 
 impl std::fmt::Display for SpellError<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let start = self.error.as_ptr() as usize - self.line.as_ptr() as usize;
         write!(
             f,
             "{}~~{}~~{}",
-            &self.line[..self.err_part.0],
-            &self.line[self.err_part.0..self.err_part.1],
-            &self.line[self.err_part.1..],
+            &self.line[..start],
+            &self.error,
+            &self.line[start + self.error.len()..],
         )
     }
 }
@@ -46,49 +41,23 @@ impl<S: SpellChecker, W: Wordifier> Root<S, W> {
         }
     }
 
-    fn spell_check_line<'a>(&self, line: String) -> SpellCheckedLine {
-        SpellCheckedLine {
-            err_parts: self
-                .wordifier // #a
-                .wordify(&line)
-                .scan(0, |acc, word| {
-                    let start = *acc;
-                    *acc += word.len() + 1;
-                    Some((start, word))
-                })
-                .filter_map(|(i, word)| {
-                    // #a: To avoid redundancy, this method could be a default
-                    // implementation on a trait (where `Self` implements the
-                    // trait). We could directly do `self.spell_check`.
-                    self.spell_checker
-                        .spell_check(word)
-                        .not()
-                        .then_some((i, i + word.len()))
-                })
-                .collect(),
-            line,
-        }
+    fn errors<'a>(&self, line: &'a str) -> Vec<&'a str> {
+        self.wordifier
+            .wordify(&line)
+            .filter_map(|word| self.spell_checker.spell_check(word).not().then_some(word))
+            .collect()
     }
 
     fn run(self) {
         stdin()
             .lines()
             .enumerate()
-            .filter_map(|(ln, line)| line.ok().map(|line| (ln + 1, self.spell_check_line(line))))
-            .for_each(
-                |(
-                    ln,
-                    SpellCheckedLine {
-                        ref line,
-                        err_parts,
-                    },
-                )| {
-                    err_parts
-                        .into_iter()
-                        .map(|err_part| SpellError { line, err_part })
-                        .for_each(|spell_err| println!("{ln}:{spell_err}"))
-                },
-            );
+            .filter_map(|(ln, line)| line.ok().map(|line| (ln + 1, line)))
+            .for_each(|(ln, ref line)| {
+                self.errors(line)
+                    .into_iter()
+                    .for_each(|error| println!("{ln}:{}", SpellError { line, error }))
+            });
     }
 }
 
