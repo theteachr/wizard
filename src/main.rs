237cf948849c90549edfe2f8b7d516b0b1e74c34
theteachr <sop.theteachr@gmail.com>
2024-11-01 01:12:25 +0530

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,26 @@
 use std::io::stdin;
+use std::ops::Not;
 
 #[derive(Debug)]
 struct SpellError<'a> {
     line: &'a str,
-    bad_part: (usize, usize),
+    err_part: (usize, usize),
 }
 
 #[derive(Debug)]
-struct BadLine {
+struct SpellCheckedLine {
     line: String,
-    bad_parts: Vec<(usize, usize)>,
-}
-
-impl BadLine {
-    fn iter_errors<'a>(&'a self) -> impl Iterator<Item = SpellError<'a>> {
-        BadLineIter {
-            line: &self.line,
-            bad_parts: &self.bad_parts,
-        }
-    }
-}
-
-struct BadLineIter<'a> {
-    line: &'a str,
-    bad_parts: &'a [(usize, usize)],
-}
-
-impl<'a> Iterator for BadLineIter<'a> {
-    type Item = SpellError<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(((start, end), rest)) = self.bad_parts.split_first() {
-            self.bad_parts = rest;
-
-            return Some(SpellError {
-                line: &self.line,
-                bad_part: (*start, *end),
-            });
-        }
-
-        None
-    }
+    err_parts: Vec<(usize, usize)>,
 }
 
 impl std::fmt::Display for SpellError<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let (start, end) = self.bad_part;
-
         write!(
             f,
             "{}~~{}~~{}",
-            &self.line[..start],
-            &self.line[start..end],
-            &self.line[end..],
+            &self.line[..self.err_part.0],
+            &self.line[self.err_part.0..self.err_part.1],
+            &self.line[self.err_part.1..],
         )
     }
 }
@@ -78,45 +46,56 @@ impl<S: SpellChecker, W: Wordifier> Root<S, W> {
         }
     }
 
-    fn spell_check_line<'a>(&self, line: String) -> Option<BadLine> {
-        let bad_parts = self
-            .wordifier
-            .wordify(&line)
-            .scan(0, |acc, word| {
-                let start = *acc;
-                *acc += word.len() + 1;
-                Some((start, word))
-            })
-            .filter_map(|(i, word)| {
-                (!self.spell_checker.spell_check(word)).then_some((i, i + word.len()))
-            })
-            .collect();
-
-        Some(BadLine { line, bad_parts })
+    fn spell_check_line<'a>(&self, line: String) -> SpellCheckedLine {
+        SpellCheckedLine {
+            err_parts: self
+                .wordifier // #a
+                .wordify(&line)
+                .scan(0, |acc, word| {
+                    let start = *acc;
+                    *acc += word.len() + 1;
+                    Some((start, word))
+                })
+                .filter_map(|(i, word)| {
+                    // #a: To avoid redundancy, this method could be a default
+                    // implementation on a trait (where `Self` implements the
+                    // trait). We could directly do `self.spell_check`.
+                    self.spell_checker
+                        .spell_check(word)
+                        .not()
+                        .then_some((i, i + word.len()))
+                })
+                .collect(),
+            line,
+        }
     }
 
     fn run(self) {
         stdin()
             .lines()
             .enumerate()
-            .filter_map(|(ln, line)| {
-                line.ok().and_then(|line| {
-                    self.spell_check_line(line)
-                        .map(|bad_line| (ln + 1, bad_line))
-                })
-            })
-            .for_each(|(ln, bad_line)| {
-                bad_line
-                    .iter_errors()
-                    .for_each(|spell_err| println!("{ln}:{spell_err}"))
-            });
+            .filter_map(|(ln, line)| line.ok().map(|line| (ln + 1, self.spell_check_line(line))))
+            .for_each(
+                |(
+                    ln,
+                    SpellCheckedLine {
+                        ref line,
+                        err_parts,
+                    },
+                )| {
+                    err_parts
+                        .into_iter()
+                        .map(|err_part| SpellError { line, err_part })
+                        .for_each(|spell_err| println!("{ln}:{spell_err}"))
+                },
+            );
     }
 }
 
 struct BadSpellChecker;
-struct BadWordifier;
+struct SimpleWordifier;
 
-impl Wordifier for BadWordifier {
+impl Wordifier for SimpleWordifier {
     fn wordify<'a, 'b>(&'a self, line: &'b str) -> impl Iterator<Item = &'b str> {
         line.split(|c: char| c.is_whitespace() || c.is_ascii_punctuation())
     }
@@ -129,5 +108,5 @@ impl SpellChecker for BadSpellChecker {
 }
 
 fn main() {
-    Root::new(BadSpellChecker, BadWordifier).run()
+    Root::new(BadSpellChecker, SimpleWordifier).run()
 }
