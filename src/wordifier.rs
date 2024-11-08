d2efb46beeddec4e750a8be5402c5e82b1dc2dc2
theteachr <sop.theteachr@gmail.com>
2024-11-09 02:50:28 +0530

    Use an iterator instead of splits
    Now a non word sequence is never passed to the spell checker

--- src/wordifier.rs
+++ src/wordifier.rs
@@ -4,9 +4,36 @@ pub trait Wordifier {
 
 pub struct SimpleWordifier;
 
+struct SimpleWordifierIter<'a> {
+    line: &'a str,
+}
+
+impl<'a> Iterator for SimpleWordifierIter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let word_start = self.line.find(char::is_alphabetic)?;
+
+        let word_end = self.line[word_start..]
+            .find(|c: char| !c.is_alphabetic() && c != '\'')
+            .map(|i| i + word_start)
+            .unwrap_or(self.line.len());
+
+        // Couldn't do the following.
+        // ```
+        // let (word, self.line) = &self.line.split_at(word_end);
+        // ```
+        // Weird error reported: Found a 3 tuple where a 2 tuple was expected.
+
+        let word = &self.line[word_start..word_end];
+        self.line = &self.line[word_end..];
+
+        Some(word)
+    }
+}
+
 impl Wordifier for SimpleWordifier {
     fn wordify<'a>(&self, line: &'a str) -> impl Iterator<Item = &'a str> {
-        line.split(|c: char| c != '\'' && (c.is_whitespace() || c.is_ascii_punctuation()))
-            .filter(|word| !word.is_empty())
+        SimpleWordifierIter { line }
     }
 }
