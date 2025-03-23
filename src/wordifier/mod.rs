ea41c3d75dccf03494401b18f391641ca2965e67
theteachr <sop.theteachr@gmail.com>
2025-03-23 18:23:04 +0530

    :memo: Document traits

--- src/wordifier/mod.rs
+++ src/wordifier/mod.rs
@@ -5,6 +5,10 @@ mod iter;
 pub use alphabetic::Alphabetic;
 pub use camel::CamelCase;
 
+/// A `Wordifier` provides an iterator over the words in a line of text.
 pub trait Wordifier {
     fn words<'a>(&self, line: &'a str) -> impl Iterator<Item = &'a str>;
 }
+
+// XXX: Maybe this should not exist.
+// We could implement `Iterator` on the types implementing `Wordifier` instead.
